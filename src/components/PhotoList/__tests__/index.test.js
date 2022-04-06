984443f91ad3fe645a6ec460c5e6edef996d08d2
dannyrgd <danny.ramirezgd@gmail.com>
2022-04-06 15:42:00 -0400

    modal tests run and pass

--- src/components/PhotoList/__tests__/index.test.js
+++ src/components/PhotoList/__tests__/index.test.js
@@ -1,4 +1,4 @@
-import Reach from 'react';
+import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import PhotoList from '..'; 
